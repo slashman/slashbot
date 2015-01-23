e237656258c823d9d462b04d83bf46ec543d03a7
slashman <java.koder@gmail.com>
2015-01-23 15:08:52 -0500

    Add sample IRC config

--- config.js
+++ config.js
@@ -1,9 +1,18 @@
 var IRCConnector = require("./classes/IRCConnector.class");
 
+// Sample Slack-thru-IRC config
 module.exports = {
 	channel: "#slashbottest",
 	server: "ffoundry.irc.slack.com",
 	botName: "slashbotx",
 	password: "milkyway",
+	connector: IRCConnector
+};
+
+// Sample plain IRC config
+module.exports = {
+	channel: "#slashbot",
+	server: "verne.freenode.net",
+	botName: "slashbot",
 	connector: IRCConnector
 };
