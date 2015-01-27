eb4a1f7c7169d06f4568fc49a27b9e9c12bd68a9
gguevara <gguevara@velocitypartners.net>
2015-01-27 06:39:52 -0500

    Changed config to use obibot as per the registered integration on Slack.

--- config.js
+++ config.js
@@ -4,27 +4,27 @@ var JSONConnector = require("./classes/persistence/JSONConnector.class");
 var MongoConnector = require("./classes/persistence/MongoConnector.class");
 
 // Sample Slack-thru-IRC config
-/*module.exports = {
-	environment: "slashieMachine",
+module.exports = {
+	environment: "platanoMachine",
 	channel: "#slashbottest",
 	server: "ffoundry.irc.slack.com",
-	botName: "slashbotx",
+	botName: "obibot",
 	password: "ffoundry.MUr0IDVbPois5VgPtbYY",
 	connector: SlackConnector,
 	token: 'xoxb-3463721915-mXLDStOphmDcva9aBNBuYCcT',
 	autoReconnect: true,
 	autoMark: true,
 	persistence: JSONConnector
-};*/
+};
 
 // Sample plain IRC config
 
-module.exports = {
-	environment: "slashieMachine",
-	channel: "#slashbot",
-	server: "verne.freenode.net",
-	botName: "slashbot",
-	connector: IRCConnector,
-	persistence: MongoConnector,
-	dbURL: 'mongodb://localhost:27017/slashbot'
-};
+// module.exports = {
+// 	environment: "slashieMachine",
+// 	channel: "#slashbot",
+// 	server: "verne.freenode.net",
+// 	botName: "obibot",
+// 	connector: IRCConnector,
+// 	persistence: MongoConnector,
+// 	dbURL: 'mongodb://localhost:27017/slashbot'
+// };
